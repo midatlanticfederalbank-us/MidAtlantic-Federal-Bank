b60facdd4acdc79d4cfe4f562f29247f2f805360
King <midatlanticfederalbank@gmail.com>
2026-08-22 15:50:36 +0300

    Update page.js

--- app/signup/page.js
+++ app/signup/page.js
@@ -4,8 +4,8 @@ import { useState } from "react";
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  process.env.next_public_supabase_url,
+  process.env.next_public_supabase_anon_key
 );
 
 export default function Signup() {
